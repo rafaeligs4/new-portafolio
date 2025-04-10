0931dfe8b050f831506098b5cd9f45fedd53c136
rafael <rafaeligs450@gmail.com>
2025-04-09 20:25:27 -0400

    09-04-2025

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../shared/components/header/header.component";
+import { CarrouselComponent } from "../../shared/components/carrousel/carrousel.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent, CarrouselComponent],
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
---
+++ src/app/shared/components/carrousel/carrousel.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrousel',
+  imports: [CommonModule],
+  templateUrl: './carrousel.component.html',
+  styleUrl: './carrousel.component.scss'
+})
+export class CarrouselComponent {
+
+}
